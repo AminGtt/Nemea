dee2f1d67c39724cd0edee3e5a2a78885cd9b6ff
Amin Gatta <gatta.amin38@gmail.com>
2024-04-10 22:06:14 +0200

    setup vue side + globalize duplicated package

---
+++ apps/SSI/src/components/ExempleComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <div>
+    Priviet blyat!
+  </div>
+
+</template>
+
+<style scoped lang="sass">
+
+  div
+    color: red
+    position: relative
+    top: 50vh
+    text-align: center
+
+</style>
--- apps/SSI/src/views/HomeView.vue
+++ apps/SSI/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import ExempleComponent from '@/components/ExempleComponent.vue';
 </script>
 
 <template>
+
   <main>
-    <TheWelcome />
+    <ExempleComponent />
   </main>
+
 </template>
