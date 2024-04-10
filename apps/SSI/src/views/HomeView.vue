f67b49e071f6c9f0de813e7e8d6af48cf1732931
Amin Gatta <gatta.amin38@gmail.com>
2024-04-11 00:09:20 +0200

    setup tresjs + scene

--- apps/SSI/src/views/HomeView.vue
+++ apps/SSI/src/views/HomeView.vue
@@ -1,11 +1,41 @@
 <script setup>
-import ExempleComponent from '@/components/ExempleComponent.vue';
+
+import { TresCanvas } from '@tresjs/core'
+import { Sphere, OrbitControls } from '@tresjs/cientos'
+
 </script>
 
 <template>
 
   <main>
-    <ExempleComponent />
+    <TresCanvas 
+      id="scene" 
+      window-size
+      clear-color="#82DBC5"
+      >
+
+      <OrbitControls />
+      
+      <Sphere :args="[2, 50, 50]" color="pink" />
+
+    </TresCanvas>
+    
+    <section id="ui">
+      <h1>Welcome to Solar System Interactive</h1>
+    </section>
   </main>
 
 </template>
+
+<style lang="sass" scoped>
+  #ui
+    z-index: 999
+    position: relative
+
+  h1
+    color: purple
+    text-align: center
+    top: 80vh
+    position: relative
+
+</style>
--- apps/SSI/vite.config.js
+++ apps/SSI/vite.config.js
@@ -3,11 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import { templateCompilerOptions } from '@tresjs/core'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      ...templateCompilerOptions,
+    }),
     VueDevTools(),
   ],
   resolve: {
